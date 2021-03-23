3b3ece04ae540cefea9aaf69968b341c5f5bf6dc
Dante <frostu8@protonmail.com>
2021-03-22 21:55:27 -0400

    initial commit

---
+++ AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.Versioning;
+using System.Security;
+using System.Security.Permissions;
+
+[assembly: TargetFramework(".NETFramework,Version=v4.0", FrameworkDisplayName = ".NET Framework 4")]
+[assembly: AssemblyCompany("frostu8")]
+[assembly: AssemblyConfiguration("Debug")]
+[assembly: AssemblyCopyright("Copyright 2021 frostu8")]
+[assembly: AssemblyDescription("Installer for Seshat.")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyProduct("SeshatInstaller.Headless")]
+[assembly: AssemblyTitle("SeshatInstaller.Headless")]
+
